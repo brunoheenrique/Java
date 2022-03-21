b64a17ea3a89d19c18176b68e0fc0d16ebf9335f
brunoheenrique <bruno180396@gmail.com>
2022-03-21 14:41:59 -0300

    testes de atividade_1

--- ProjetoTesteJava/src/App.java
+++ ProjetoTesteJava/src/App.java
@@ -1,10 +1,16 @@
 import estruturasTeste.*;
 
+class B{}
+class C{}
+class D extends B{}
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        primeiroTeste teste = new primeiroTeste();
-        teste.chamaPrint();
-        segundoTeste teste2 = new segundoTeste();
-        teste2.chamaPrintNovo();
+    static int a(D d, B b2) { return 1; }
+    static int a(C c, C c2) { return 2; }
+    static int a(B b, B b2) { return 3; }
+    static int a(App a, App a2) { return 4; }
+    public static void main(String[] args) {
+        System.out.println(a(new D(), new D()));
     }
 }
+
