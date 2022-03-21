0b04860a2be58c38c27f444e531da60f6533838c
brunoheenrique <bruno180396@gmail.com>
2022-03-21 15:36:47 -0300

    códigosdeTeste

--- ProjetoTesteJava/src/App.java
+++ ProjetoTesteJava/src/App.java
@@ -1,16 +1,7 @@
 import estruturasTeste.*;
-
-class B{}
-class C{}
-class D extends B{}
-
 public class App {
-    static int a(D d, B b2) { return 1; }
-    static int a(C c, C c2) { return 2; }
-    static int a(B b, B b2) { return 3; }
-    static int a(App a, App a2) { return 4; }
     public static void main(String[] args) {
-        System.out.println(a(new D(), new D()));
+       
     }
 }
 
