dca6d22673189938dba162f7ed00974bfe245b68
brunoheenrique <bruno180396@gmail.com>
2022-03-28 15:45:08 -0300

    testes na classe App

--- ProjetoTesteJava/src/App.java
+++ ProjetoTesteJava/src/App.java
@@ -1,7 +1,14 @@
-import classesPend.Conta;
+import classesPend.*;
 public class App {
     public static void main(String[] args) {
-        
+        int[] i = {150, 151};
+        i = s(i);
+        System.out.println(i[1]);
+    }
+    static int[] s(int[] i) {
+        int[] j = {i[0], i[1]};
+        i[1]++;
+        return j;
     }
 }
 
