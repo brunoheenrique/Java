394bab0cf415f1f151800f6d5acf287503680218
brunoheenrique <bruno180396@gmail.com>
2022-03-31 09:51:31 -0300

    mais testes na classe App

--- ProjetoTesteJava/src/App.java
+++ ProjetoTesteJava/src/App.java
@@ -1,20 +1,2 @@
-class D extends C {
-    void x() { System.out.println(1); }
-    void y(C b) {
-        x();
-    }
-}
-class C extends B {
-    void x() { System.out.println(2); }
-}
-class B {
-    void x() { System.out.println(3); }
-    void y(B b) {
-        b.x();
-    }
-}
-public class App {
-    public static void main(String[] args) {
-        new D().y(new C());
-    }
-}
+import java.io.*
+
