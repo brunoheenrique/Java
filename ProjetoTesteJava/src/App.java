053b71f63dfa5835b20bd1528fa09294560213de
brunoheenrique <bruno180396@gmail.com>
2022-03-31 10:12:30 -0300

    novos testes na classe App

--- ProjetoTesteJava/src/App.java
+++ ProjetoTesteJava/src/App.java
@@ -1,2 +1,38 @@
-import java.io.*
+import java.io.*;
 
+class Conta{
+    private int id;
+    
+    protected void abrir() {
+        System.out.println("abrindo conta");
+    }
+    
+    private void someu() {
+       System.out.println("este é o meu método"); 
+    }
+    
+}  
+
+class ContaCorrente extends Conta{
+    
+    public void exibeConta () {
+        
+    }
+       
+}
+
+public class App {
+    public static void main(String[] args) {
+        Conta conta = new ContaCorrente();
+              
+        conta.abrir();
+        
+        for(int i = 1;i<=10;i++){
+            System.out.println("O numero exibido no for e: " + i);
+            i +=1;
+        }
+        
+    }
+    
+}
+    
