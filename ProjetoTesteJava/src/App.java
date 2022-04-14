97549dc698b3d39a592656983b2b012d173ca134
brunoheenrique <bruno180396@gmail.com>
2022-04-14 13:51:10 -0300

    testes printf

--- ProjetoTesteJava/src/App.java
+++ ProjetoTesteJava/src/App.java
@@ -2,7 +2,7 @@ import java.util.*;
 import models.*;
 public class App {
     public static void main(String[]args){
-          
+          System.out.printf("[%-+,14.2f] %n", 18965342.99);
     }
         
 }
