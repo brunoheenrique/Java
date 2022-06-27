8714d7fbec5f620897316dddb6cf17e1d7baef61
brunoheenrique <bruno180396@gmail.com>
2022-06-27 16:50:40 -0300

    alterando manifesto e construindo fluxo correto da aplicação

--- Android/Agenda/app/src/main/java/com/example/agenda/ui/activity/ListaAlunoActivity.java
+++ Android/Agenda/app/src/main/java/com/example/agenda/ui/activity/ListaAlunoActivity.java
@@ -1,5 +1,6 @@
 package com.example.agenda.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agenda.R;
 import com.example.agenda.dao.AlunoDAO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListaAlunoActivity extends AppCompatActivity {
 
@@ -17,6 +19,12 @@ public class ListaAlunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
+
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_botao_novo_aluno);
+        botaoNovoAluno.setOnClickListener(view ->
+            startActivity(new Intent(ListaAlunoActivity.this,
+                    FormularioAlunoActivity.class))
+        );
 
         AlunoDAO dao = new AlunoDAO();
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
