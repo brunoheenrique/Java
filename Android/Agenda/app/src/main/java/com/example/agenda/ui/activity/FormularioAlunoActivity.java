39981e7802e3be2dfcba9c80648599012b66d1b0
brunoheenrique <bruno180396@gmail.com>
2022-06-27 15:18:35 -0300

    inserindo ação OnListener, e refatorando com lamba

--- Android/Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ Android/Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,8 +1,10 @@
 package com.example.agenda.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agenda.R;
 
@@ -12,5 +14,9 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        setTitle("Novo Aluno");
+        Button botaosalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaosalvar.setOnClickListener(view -> Toast.makeText(FormularioAlunoActivity.this,
+                "Salvou !",Toast.LENGTH_SHORT).show());
     }
 }
