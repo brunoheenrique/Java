968f0f62d49737b9b564d8e98f8780098536457a
brunoheenrique <bruno180396@gmail.com>
2022-06-27 15:57:15 -0300

    adicionando classe Aluno e interagindo com Botao

--- Android/Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ Android/Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,11 +2,13 @@ package com.example.agenda.ui.activity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agenda.R;
+import com.example.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -15,8 +17,21 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
         setTitle("Novo Aluno");
+        EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
         Button botaosalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
-        botaosalvar.setOnClickListener(view -> Toast.makeText(FormularioAlunoActivity.this,
-                "Salvou !",Toast.LENGTH_SHORT).show());
+        botaosalvar.setOnClickListener(view -> {
+            String nome = campoNome.getText().toString();
+            String telefone = campoTelefone.getText().toString();
+            String email = campoEmail.getText().toString();
+
+        Aluno alunoCriado = new Aluno(nome,telefone,email);
+            Toast.makeText(FormularioAlunoActivity.this,alunoCriado.getNome() +
+                    " - " + alunoCriado.getTelefone()+
+                    " - " + alunoCriado.getEmail(),Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
