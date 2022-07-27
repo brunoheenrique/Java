61dfea4c7b66d17fed814652b4af66ce2df3b21f
brunoheenrique <bruno180396@gmail.com>
2022-07-26 21:20:27 -0300

    implementando Application

--- Android/Agenda/app/src/main/java/com/example/agenda/AgendaApplication.java
+++ Android/Agenda/app/src/main/java/com/example/agenda/AgendaApplication.java
@@ -10,8 +10,12 @@ public class AgendaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        criaAlunosTeste();
+    }
+
+    private void criaAlunosTeste() {
         AlunoDAO dao = new AlunoDAO();
-        dao.salvar(new Aluno("Bruno", "123123", "bruno@teste.com"));
-        dao.salvar(new Aluno("Jebises", "123123123", "jefrio@teste.com"));
+        dao.salvar(new Aluno("Bruno", "344779233", "bruno@teste.com"));
+        dao.salvar(new Aluno("Josimar", "123462143", "jefrio@teste.com"));
     }
 }
