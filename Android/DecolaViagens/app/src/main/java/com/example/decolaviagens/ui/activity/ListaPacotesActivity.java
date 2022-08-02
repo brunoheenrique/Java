1774d3a8d4d4f0eaac6362d7b819135946158d7f
brunoheenrique <bruno180396@gmail.com>
2022-08-02 20:12:36 -0300

    aplicando primeiras strings e conhecimentos de constraint em xml e visual

---
+++ Android/DecolaViagens/app/src/main/java/com/example/decolaviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,18 @@
+package com.example.decolaviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.decolaviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+
+
+}
