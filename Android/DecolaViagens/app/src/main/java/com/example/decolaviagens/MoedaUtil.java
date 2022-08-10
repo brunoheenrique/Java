eeb6fc76d0ff3338f17b98ee97e05f6530db6f42
brunoheenrique <bruno180396@gmail.com>
2022-08-09 23:46:52 -0300

    refatorando codigos java e layout

--- Android/DecolaViagens/app/src/main/java/com/example/decolaviagens/MoedaUtil.java
+++ Android/DecolaViagens/app/src/main/java/com/example/decolaviagens/MoedaUtil.java
@@ -9,12 +9,17 @@ import java.util.Locale;
 
 public class MoedaUtil {
 
+    public static final String PORTUGUES = "pt";
+    public static final String BRASIL = "br";
+    public static final String FORMATO_PADRAO = "R$";
+    public static final String FORMATO_NOVO = "R$ ";
+
     @NonNull
     public static String formataPreco(BigDecimal valor) {
-        NumberFormat formatoBR = DecimalFormat.getCurrencyInstance(new Locale("pt", "br"));
+        NumberFormat formatoBR = DecimalFormat.getCurrencyInstance(new Locale(PORTUGUES, BRASIL));
         return formatoBR
                 .format(valor)
-                .replace("R$", "R$ ");
+                .replace(FORMATO_PADRAO, FORMATO_NOVO);
     }
 
 }
