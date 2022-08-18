de7094515631928849a25df93a7f7e5670260bce
brunoheenrique <bruno180396@gmail.com>
2022-08-18 19:59:42 -0300

    reorganizando projetos, e iniciando recycleView

---
+++ Android/Ceep/app/src/main/java/com/br/ceep/views/activity/ListaNotasActivity.java
@@ -0,0 +1,16 @@
+package com.br.ceep.views.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.ceep.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
