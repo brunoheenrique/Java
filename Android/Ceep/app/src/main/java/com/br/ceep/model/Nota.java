64b9374be8a2f3e4801d1b15b904c563259d91bb
brunoheenrique <bruno180396@gmail.com>
2022-08-18 21:29:47 -0300

    reorganizando projetos, e iniciando recycleView

---
+++ Android/Ceep/app/src/main/java/com/br/ceep/model/Nota.java
@@ -0,0 +1,21 @@
+package com.br.ceep.model;
+
+public class Nota {
+
+    private final String titulo;
+    private final String descricao;
+
+    public Nota(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
--- Android/Ceep/app/src/main/java/com/br/ceep/views/activity/ListaNotasActivity.java
+++ Android/Ceep/app/src/main/java/com/br/ceep/views/activity/ListaNotasActivity.java
@@ -3,8 +3,14 @@ package com.br.ceep.views.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.br.ceep.R;
+import com.br.ceep.controller.NotaDAO;
+import com.br.ceep.model.Nota;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +18,11 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.lista_notas_listview);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira nota","Vai a merda pacero"));
+
     }
 }
