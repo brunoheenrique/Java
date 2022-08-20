61022cac5898f0bea3e082758557eb8746c588e0
brunoheenrique <bruno180396@gmail.com>
2022-08-20 15:01:01 -0300

    inserindo segunda activity ja com layout personalizado

---
+++ Android/Ceep/app/src/main/java/com/br/ceep/views/activity/FormularioNotaActivity.java
@@ -0,0 +1,16 @@
+package com.br.ceep.views.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.ceep.R;
+
+public class FormularioNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_nota);
+    }
+}
