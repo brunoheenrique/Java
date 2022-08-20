c853e71de6253bb1a6f295c682a8573f8cfbae96
brunoheenrique <bruno180396@gmail.com>
2022-08-20 15:41:12 -0300

    inserindo segunda activity ja com layout personalizado

--- Android/Ceep/app/src/main/java/com/br/ceep/views/activity/FormularioNotaActivity.java
+++ Android/Ceep/app/src/main/java/com/br/ceep/views/activity/FormularioNotaActivity.java
@@ -1,10 +1,12 @@
 package com.br.ceep.views.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.br.ceep.R;
 
@@ -20,5 +22,14 @@ public class FormularioNotaActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(@NonNull Menu menu) {
         getMenuInflater().inflate(R.menu.menu_formulario_salva_nota, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_formulario_ic_salva){
+            EditText titulo_nota = findViewById(R.id.formulario_nota_titulo);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
