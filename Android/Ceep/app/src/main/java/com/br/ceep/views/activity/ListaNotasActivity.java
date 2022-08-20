e7b4f1fc49dadc8739b69a34c3d13575895d7d3c
brunoheenrique <bruno180396@gmail.com>
2022-08-20 15:16:20 -0300

    inserindo segunda activity ja com layout personalizado

--- Android/Ceep/app/src/main/java/com/br/ceep/views/activity/ListaNotasActivity.java
+++ Android/Ceep/app/src/main/java/com/br/ceep/views/activity/ListaNotasActivity.java
@@ -1,6 +1,8 @@
 package com.br.ceep.views.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,12 +23,19 @@ public class ListaNotasActivity extends AppCompatActivity {
 
         List<Nota> todasNotas = notasDeExemplo();
         configuraRecyclerView(todasNotas);
+
+        TextView InsereNota = findViewById(R.id.lista_notas_insere_nota);
+        InsereNota.setOnClickListener(view -> {
+            Intent iniciaFormularioNota = new Intent(ListaNotasActivity.this,
+                    FormularioNotaActivity.class);
+            startActivity(iniciaFormularioNota);
+        });
     }
 
     private List<Nota> notasDeExemplo() {
         NotaDAO dao = new NotaDAO();
-        dao.insere(new Nota("Primeira nota do cadernin", "Ah vai se lasca n quero saber"),
-                new Nota("Segunda nota do cadernin", "Man, tu é insistente na parada"));
+        dao.insere(new Nota("Primeira nota do cadernin", "Nota"),
+                new Nota("Segunda nota do cadernin", "Nota"));
         return dao.todos();
     }
 
